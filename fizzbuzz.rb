77a05fe32b5179e1e80e6dc032ab4627e32ae25f
Greg Stimson <stimsong@yahoo.com>
2018-02-03 22:51:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(x)
-  if x % 3 == 0
+  if (x % 3 == 0) && (x % 5 == 0)
+    fizz_15 = "FizzBuzz"
+  elsif x % 3 == 0
     fizz_3 = "Fizz"
   elsif x % 5 == 0
     fizz_5 = "Buzz"
-  elsif (x % 3 == 0) && (x % 5 == 0)
-    fizz_15 = "FizzBuzz"
   end
 end
